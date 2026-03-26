4ddc5e41682a804e6f5c8e8e49c28638484662bc
MiloszBratkowski <milosz.bratkowski.main@gmail.com>
2026-03-26 11:43:06 +0100

    Simple logger implementation

---
+++ PJATK-APBD-RENTAL/Infrastructure/ConsoleLogger.cs
@@ -0,0 +1,7 @@
+﻿namespace PJATK_APBD_RENTAL.Reports;
+
+public class ConsoleLogger : ILogger
+{
+    public void LogInfo(string message) => Console.WriteLine($"[INFO] {message}");
+    public void LogError(string message) => Console.WriteLine($"[ERR] {message}");
+}
